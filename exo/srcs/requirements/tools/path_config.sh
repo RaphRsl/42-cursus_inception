0ace78b922e5ceaab1be72733b42c85c54d91da9
Raphael ROUSSEL <rroussel@made-f0Br3s12.clusters.42paris.fr>
2024-06-13 16:13:03 +0200

    g_save_42

---
+++ exo/srcs/requirements/tools/path_config.sh
@@ -0,0 +1,16 @@
+echo "=== INCEPTION PATH CONFIGURATION ==="
+
+printf "\nPath for mariadb database and wordpress files:"
+printf "\nExemple : /mnt/nfs/homes/rroussel/data\n"
+read path
+
+if [ ! -d "$path" ]; then
+    echo "Invalid path."
+    exit 1
+fi
+
+echo "$path" > srcs/requirements/tools/data_path.txt
+
+cp srcs/requirements/tools/template_compose.yml srcs/docker-compose.yml
+cat srcs/docker-compose.yml | sed "s+pathtodata+$path+g" > srcs/docker-compose.yml.tmp
+mv srcs/docker-compose.yml.tmp srcs/docker-compose.yml
